307574ed12e3e82b0afa93b5ac41164c205120d9
5cm/s <you@example.com>
2025-05-01 18:21:13 -0800

    StackT

---
+++ Head/Class/StrackT.h
@@ -0,0 +1,36 @@
+#ifndef STRACKT_H
+#define STRACKT_H
+
+#include <vector>
+#include <stdexcept>
+using namespace std;
+template <typename T>
+class Stack {
+    vector<T> d;
+
+    public:
+    void push(const T& value) {
+        d.push_back(value);
+    }
+
+    void pop() {
+        if (d.empty()) throw out_of_range("Stack is empty");
+        d.pop_back();
+    }
+
+    T& top() {
+        if (d.empty()) throw out_of_range("Stack is empty");
+        return d.back();
+    }
+
+    bool empty() const {
+        return d.empty();
+    }
+
+    size_t size() const {
+        return d.size();
+    }
+};
+
+
+#endif
